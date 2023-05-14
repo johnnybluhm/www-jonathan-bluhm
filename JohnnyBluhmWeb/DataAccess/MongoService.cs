7302f73b1ca006637bbabb61122e9a22519c28f2
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2023-05-14 13:18:44 -0600

    Add mongo service skeleton

--- JohnnyBluhmWeb/DataAccess/MongoService.cs
+++ JohnnyBluhmWeb/DataAccess/MongoService.cs
@@ -1,6 +1,36 @@
-﻿namespace JohnnyBluhmWeb.DataAccess
+﻿using JohnnyBluhmWeb.Models;
+using MongoDB.Driver;
+
+namespace JohnnyBluhmWeb.DataAccess
 {
     public class MongoService
     {
+        private MongoClient mongoClient;
+        private const string connectionUri = "mongodb://localhost:27017";
+        private const string activitiesString = "activities";
+        private const string detailedActivitiesString = "detailedActivities";
+        private const string streamsString = "streams";
+        private IMongoDatabase stravaDb;
+        public IMongoCollection<StravaActivity> activitiesCollection;
+        public IMongoCollection<DetailedActivity> detailedActivitiesCollection;
+        public IMongoCollection<ActivityStream> streamCollection;
+
+        public MongoService()
+        {
+
+        }
+
+        private void SetUpMongo()
+        {
+            var settings = MongoClientSettings.FromConnectionString(connectionUri);
+            // Set the ServerApi field of the settings object to Stable API version 1
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            // Create a new client and connect to the server
+            var client = new MongoClient(settings);
+            stravaDb = mongoClient.GetDatabase("strava");
+            activitiesCollection = stravaDb.GetCollection<StravaActivity>(activitiesString);
+            detailedActivitiesCollection = stravaDb.GetCollection<DetailedActivity>(detailedActivitiesString);
+            streamCollection = stravaDb.GetCollection<ActivityStream>(streamsString);
+        }
     }
 }
