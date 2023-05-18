bc15e4abff4dff1020fa4436d366184055f7fcc9
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2023-05-18 00:58:46 -0600

    Add getbyId methods

--- JohnnyBluhmWeb/Controllers/StravaMongoDB.cs
+++ JohnnyBluhmWeb/Controllers/StravaMongoDB.cs
@@ -3,6 +3,7 @@ using JohnnyBluhmWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
 using Newtonsoft.Json;
+using System.Diagnostics;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -47,6 +48,30 @@ namespace JohnnyBluhmWeb.Controllers
             var filter = Builders<PowerStream>.Filter.Empty;
             var result = await mongoService.powerCollection.FindAsync<PowerStream>(filter);
             return JsonConvert.SerializeObject(result.ToList());
+        }
+
+        [HttpGet]
+        [Route("GetPowerStreamById/{id}")]
+        public async Task<string> GetAllPowerStreams([FromRoute] long id)
+        {
+            var filter = Builders<PowerStream>.Filter;
+            var result = await mongoService.powerCollection.FindAsync<PowerStream>(e =>e.id == id);
+            return JsonConvert.SerializeObject(result.ToList());
+        }
+
+        [HttpGet]
+        [Route("timeTest/{id}")]
+        public async Task<string> TimeTest([FromRoute] long id)
+        {
+            var filter = Builders<PowerStream>.Filter.Empty;
+            var result = await mongoService.powerCollection.FindAsync<PowerStream>(filter);
+
+            var list = result.ToList();
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            list.Find(e => e.id == id);
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds.ToString();
         }
     }
 }
