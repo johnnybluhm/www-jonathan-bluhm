4ab2ddc420f07f88c36d03ab039e37d460f8463b
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2023-05-18 01:52:52 -0600

    Add bad ts but working js

---
+++ JohnnyBluhmWeb/wwwroot/src/JavaScript.js
@@ -0,0 +1,9 @@
+﻿async function main() {
+    var url = "https://localhost:7038/api/stravaMongo/GetAllPowerStreams";
+
+    var result = await fetch(url);
+    var data = await result.json();
+    console.log(data);
+}
+
+main();
---
+++ JohnnyBluhmWeb/wwwroot/src/index.ts
@@ -0,0 +1,13 @@
+﻿import { PowerStream } from "./models/powerStream";
+
+
+
+async function main() {
+    var url = "https://localhost:7038/api/stravaMongo/GetAllPowerStreams";
+
+    var result = await fetch(url);
+    var data = await result.json() as PowerStream[];
+    console.log(data);
+}
+
+main();
